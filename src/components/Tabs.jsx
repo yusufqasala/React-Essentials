1e27a1a220f3f591c83ad87425a25de87926c534
yusufqasala <140950647+yusufqasala@users.noreply.github.com>
2023-12-20 17:18:47 +0200

    Add files via upload

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,9 @@
+export default function Tabs({ children, buttons, ButtonsContainer }) {
+//   const ButtonsContainer = buttonsContainer;
+  return (
+    <>
+      <ButtonsContainer>{buttons}</ButtonsContainer>
+      {children}
+    </>
+  );
+}
